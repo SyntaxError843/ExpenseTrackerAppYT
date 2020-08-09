8489b44b4dc1f4331b5fbaa105f37c02ff30c4dd
Salah Ghomrawi <ghoumrawi2020@gmail.com>
2020-08-09 23:54:47 +0300

    added Balance to UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
